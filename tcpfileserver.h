7bb62f12749e4b2d63d1fe0f1f7fe967278319f2
HSU,WEI-YU <41343121@nfu.edu.tw>
2025-11-28 20:18:08 +0800

    A

---
+++ tcpfileserver.h
@@ -0,0 +1,36 @@
+#ifndef TCPFILESERVER_H
+#define TCPFILESERVER_H
+
+#include <QDialog>
+#include <QtNetwork>
+#include <QtWidgets>
+
+class TcpFileServer : public QDialog
+{
+    Q_OBJECT
+
+public:
+    TcpFileServer(QWidget *parent = nullptr);
+    ~TcpFileServer();
+public slots:
+    void start();
+    void acceptConnectiom();
+    void updateServerProgress();
+    void displayError(QAbstractSocket::SocketError socketError);
+private:
+    QProgressBar    *serverProgressBar;
+    QLabel          *serverStatusLabel;
+    QPushButton     *startButton;
+    QPushButton     *quitButton;
+    QDialogButtonBox    *buttonBox;
+
+    QTcpServer      tcpServer;
+    QTcpSocket      *tcpServerConnection;
+    qint64          totalBytes;
+    qint64          byteReceived;
+    qint64          fileNameSize;
+    QString         fileName;
+    QFile           *localFile;
+    QByteArray      inBlock;
+};
+#endif // TCPFILESERVER_H
